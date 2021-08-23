74dcbf31997effaa83baa45029bd4e7657d45ad3
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-23 11:27:32 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,8 +1,16 @@
-import math
 def main():
     #escribe tu código abajo de esta línea
+    #El mensaje para recibir el dato debe ser **"Dame un número: **"
+    #El mensaje para la salida debe ser **"Es positivo, Es negativo ó Es cero **"
+    num = int(input('Dame un número: '))
+
+    if(num==0):
+        print('Es cero')
+    if(num>0):
+        print('Es positivo')
+    if(num<0):
+        print('Es negativo')
     pass
-    
 
 if __name__ == '__main__':
     main()
