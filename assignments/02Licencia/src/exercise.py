a2be77e7b27a64ae751f63061b3b327e60588646
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-23 13:18:58 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,21 @@
-
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    # Escribe el código adecuado para completar el programa
+    # Para pedir el dato de la idetificación oficial emplea este mensaje:
+    # "¿Tienes identificación oficial? (s/n): "
 
+    if(edad>=18):
+       sino = input('¿Tienes identificación oficial? (s/n): ')
+       if(sino=="s"):
+           print('Trámite de licencia concedido')
+       elif(sino=="n"):
+           print('No cumples requisitos')
+       else:
+           print('Respuesta incorrecta')  
+    else:
+        print('No cumples requisitos')
+
+    pass
 
 if __name__ == '__main__':
     main()
